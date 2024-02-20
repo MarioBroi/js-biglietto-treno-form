97b346b55d860b9f0173a14bf63bb180b9cd6ef6
MarioBroi <inoix16@gmail.com>
2024-02-20 14:05:00 +0000

    edit ticket price calculation

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,15 +6,16 @@ buttonElement.addEventListener('click', function () {
     const km = document.getElementById('km').value;
     const age = document.getElementById('age').value;
     console.log(km, age); //km and age
+    //- Calculate of the standard price for the ticket (km * 0.21)
+    const pricePerKm = 0.21;
+    const discount20 = 0.2;
+    const discount40 = 0.4;
+
+    let ticketPrice = (km * pricePerKm);
+    console.log(ticketPrice); //Price of the ticket
 });
 
-//- Calculate of the standard price for the ticket (km * 0.21)
-const pricePerKm = 0.21;
-const discount20 = 0.2;
-const discount40 = 0.4;
 
-let ticketPrice = km * pricePerKm;
-console.log(ticketPrice);
 
 //- Client age verification
 //    - IF the client is under 18 yh i will apply a 20% discount on the standard ticket price
